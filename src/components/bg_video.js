22e0be27bc4b699450b3c649ff805fa89bd35181
Qingfan Wu <wqfan555@gmail.com>
2017-08-23 08:45:51 +0800

    Add fan video

--- src/components/bg_video.js
+++ src/components/bg_video.js
@@ -2,11 +2,17 @@ import React from 'react'
 import './bg_video.css'
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const backgrounds = [
+  ['water.jpg', 'water.mp4'],
+  ['fan.jpg', 'fan.mp4'],
+];
 
 function BgVideo() {
+  const bg_index = Math.floor(Math.random() * backgrounds.length);;
+  const [bg_pic, bg_video] = backgrounds[bg_index];
   return (
     <div className="fullscreen-bg">
-      <video loop autoPlay muted playsInline className="fullscreen-bg__video" poster={PUBLIC_URL + '/water.jpg'} src={PUBLIC_URL + '/water.mp4'} type="video/mp4" />
+      <video loop autoPlay muted playsInline className="fullscreen-bg__video" poster={`${PUBLIC_URL}/${bg_pic}`} src={`${PUBLIC_URL}/${bg_video}`} type="video/mp4" />
     </div>
   );
 }
